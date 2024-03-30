0666cbf599c45ec4454107d7af6a70946439bf75
Kunwar-bir-singh <kunwar.bir21@gmail.com>
2024-03-30 13:56:06 +0530

    Changes

--- Random1/src/DoubleLinkedList.java
+++ Random1/src/DoubleLinkedList.java
@@ -51,8 +51,65 @@ public class DoubleLinkedList {
         newNode.next = null;
         newNode.prev = temp;
         temp.next = newNode;
+        size++;
+    }
 
+    public void insertIndex(int value , int index){
+        if(size <=1 || index == 0){
+            insertFirst(value);
+            return;
+        }
+        if(size == index){
+            inserLast(value);
+            return;
+        }
+        Node temp = head;
+        for(int i = 0 ; i < index ; i++){
+            temp = temp.next;
 
+        }
+        System.out.println(temp.value);
+        Node before = temp.prev;
+        Node newNode = new Node(value);
+
+        before.next = newNode;
+        newNode.next = temp;
+        temp.prev = newNode;
+        newNode.prev = before;
+        size++;
+    }
+
+    public void deleteFirst(){
+        if(size <=0){
+            System.out.println("Linked List Already Empty");
+            return;
+        }
+        System.out.println("Value Deleted :" + head.value);
+        head = head.next;
+        head.prev = null;
+        size--;
+    }
+
+    public void deleteLast(){
+        if(size <=0){
+            deleteFirst();
+            return;
+        }
+        Node temp = head;
+        for(int i =0 ; i < size-2 ; i++){
+            temp = temp.next;
+        }
+        System.out.println("Value Deleted :" + temp.next.value);
+        temp.next.prev = null;
+        temp.next = null;
+        size--;
+    }
+
+    public void deleteIndex(int index){
+        Node before = head;
+        for(int i = 0 ; i < index-1 ; i++){
+
+        }
     }
     private class Node{
         private int value;
