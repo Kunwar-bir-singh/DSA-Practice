36cef4c8d4e93e2ff65953b352be73a286d2fc66
Kunwarbir <kunwarbir@shauryasoft>
2024-12-31 17:22:29 +0530

    40. Combination Sum II

--- DSA/src/Recursion/Leetcode40.java
+++ DSA/src/Recursion/Leetcode40.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int climbStairs(int n) {
-        int currStep = 2;
-        int prevStep = 1;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ansList = new ArrayList<>();
+        List <Integer> currList = new ArrayList<>();
 
-        if(n == 1) return prevStep;
-        if(n == 2) return currStep;
-
-        for(int i = 3 ; i <=n ; i++){
-            int addedSteps = currStep + prevStep;
-            prevStep = currStep;
-            currStep = addedSteps;
+        Arrays.sort(candidates);
+        calculateSum(candidates, target, ansList, currList, 0, 0);
+        
+        return ansList;
+    }
+    public static void calculateSum(int[] candidates, int target, List<List<Integer>> ansList, List<Integer> currList, int sum, int index){
+        if(sum >= target){
+            if(sum == target) ansList.add(new ArrayList<>(currList));
+            return;
         }
-        return currStep;
+        for(int i = index ; i < candidates.length ; i++ ){
+            if (i > index && candidates[i] == candidates[i-1]) continue;
+
+            currList.add(candidates[i]);
+
+            calculateSum(candidates, target, ansList, currList, sum + candidates[i], i + 1);
+            
+            currList.remove(currList.size() - 1);
+        }
     }
 }
